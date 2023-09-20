e645478693b9cc07b09a930ce8d39c95a7329b6c
Naoya Minomura <naotoitoi@gmail.com>
2023-09-20 15:27:52 +0900

    modify ip

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.set('trust proxy',1);
+  
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
